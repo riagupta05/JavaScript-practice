ec6ed653cddd6747cefb9b7b9543605d7a457710
Ria <riagupta031105@gmail.com>
2026-03-23 15:20:56 +0530

    added user input

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let num = 9;
-if (num % 2 === 0) {
-    console.log("Even");
+const prompt = require("prompt-sync")();
+let a = Number(prompt("Enter a: "));
+let b = Number(prompt("Enter b: "));
+let c = Number(prompt("Enter c: "));
+if ( a>b && a> c) {
+    console.log( a + " is largest");
+} else if ( b > a && b> c) {
+    console.log( b + " is largest");
+} else if ( c > b && c> a ){
+    console.log(c + " is largest");
+} else if ( a === b && a> c ) {
+    console.log( a + " and " + b + " both are largest");
+} else if ( a===c && a>b) {
+    console.log(a + " and "+ c + " both are largest");
+} else if ( b===c && b>a) {
+    console.log( b + " and " + c + " both are largest");
 } else {
-    console.log("Odd");
+    console.log( a + " and " + b + " and " + c + " all are equal");
 }
